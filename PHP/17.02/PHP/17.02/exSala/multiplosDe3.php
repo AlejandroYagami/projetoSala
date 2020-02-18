90557fc3c7909b4e867f67f908336b0dace4c894
AlejandroYagami <57274123+AlejandroYagami@users.noreply.github.com>
2020-02-17 22:34:47 -0300

    pasta att

---
+++ PHP/17.02/PHP/17.02/exSala/multiplosDe3.php
@@ -0,0 +1,6 @@
+<?php
+    for($x = 3; $x <= 100; $x+=3){
+        $resto = $x;
+        echo $resto."<br>";
+    }
+?>
---
+++ PHP/17.02/PHP/17.02/exSala/soma1000.php
@@ -0,0 +1,7 @@
+<?php
+$soma = 0;
+    for($y = 1; $y <= 1000; $y++){
+        $soma = $soma + $y;
+    }
+    echo $soma."<br>";
+        ?>
